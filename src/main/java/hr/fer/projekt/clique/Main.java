4ceb9e080b5f27f023737cb3b9edd7ca59919ff2
Ante Spajic <ante.spajic@yahoo.com>
2016-12-20 23:28:55 +0100

    pulling

--- src/main/java/hr/fer/projekt/clique/Main.java
+++ src/main/java/hr/fer/projekt/clique/Main.java
@@ -1,8 +1,21 @@
 package hr.fer.projekt.clique;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import org.jgrapht.alg.BronKerboschCliqueFinder;
+
 public class Main {
 
 	public static void main(String[] args) {
 		
+		try {
+			Files.readAllLines(Paths.get("./testFile.txt"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		BronKerboschCliqueFinder<V, E>
 	}
 }
